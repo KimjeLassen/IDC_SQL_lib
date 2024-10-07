5f51677b107a95743291cf3bb430486ea2bc4a3d
KimjeLassen <kilas21@student.sdu.dk>
2024-10-07 13:19:04 +0200

    Moved the models into their own folder

--- print.py
+++ print.py
@@ -1,4 +1,9 @@
-import rolegroup_model
+# some_file.py
+import sys
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, 'models')
+
+from models import rolegroup_model
 
 rolegroup_model.add_group()
 rolegroup_model.print_group_name_and_desc()
