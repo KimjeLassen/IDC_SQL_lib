e214a69bffb73299ce5f590ec090f6d746c6bf2d
AGlering <algle11@student.sdu.dk>
2024-11-04 13:40:52 +0000

    cleanup

--- models/user_roles_mapping_model.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String, ForeignKey, JSON
-from db_base import Base, session
-
-class User_Roles_Mapping(Base):
-    __tablename__ = 'user_roles_mapping'
-    user_id = Column(String, ForeignKey('users.id'))
-    user_role_id = Column(String, ForeignKey('userrole.id'))
-    meta_data = Column(JSON)
