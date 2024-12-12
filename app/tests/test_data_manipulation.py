9c29763a03360a0abf78021b95a4051b087feff0
KimjeLassen <kimjelassen@yahoo.com>
2024-12-12 11:19:12 +0100

    Removed perform_db_scan

--- app/tests/test_data_manipulation.py
+++ app/tests/test_data_manipulation.py
@@ -43,45 +43,6 @@ class TestDataManipulation(unittest.TestCase):
         self.assertEqual(result.loc[2, "role3"], 1)
         self.assertEqual(result.loc[3, "role2"], 1)
 
-   # def test_perform_kmeans_and_hierarchical(self):
-   #     """
-   #     Test the `perform_kmeans_and_hierarchical` function to ensure it correctly
-   #     applies KMeans and hierarchical clustering.
-
-   #     Asserts:
-   #     - The length of the cluster labels matches the number of data points.
-   #     """
-   #     # Arrange: Create a sample input array
-   #     data = np.array([[0, 1], [1, 0], [0, 1]])
-   #     n_clusters = 2
-
-   #     # Act: Call the function under test
-   #     kmeans_labels, hierarchical_labels = perform_kmeans_and_hierarchical(
-   #         data, n_clusters
-   #     )
-
-   #     # Assert: Verify the output labels
-   #     self.assertEqual(len(kmeans_labels), len(data))
-   #     self.assertEqual(len(hierarchical_labels), len(data))
-
-   # def test_perform_kmeans_and_hierarchical_invalid_input(self):
-   #     """
-   #     Test the `perform_kmeans_and_hierarchical` function with invalid input data
-   #     to ensure it raises a `ValueError` when the input array is empty.
-
-   #     Asserts:
-   #     - A `ValueError` is raised when clustering is performed on an empty array.
-   #     """
-   #     # Arrange: Use an invalid input (empty array)
-   #     data = np.array([]).reshape(0, 2)
-   #     n_clusters = 2
-
-   #     # Act and Assert: Verify that the function raises a ValueError
-   #     with self.assertRaises(
-   #         ValueError, msg="Expected ValueError for empty input array"
-   #     ):
-   #         perform_kmeans_and_hierarchical(data, n_clusters)
-
     def test_perform_dbscan(self):
         """
         Test the `perform_dbscan` function to ensure it correctly applies DBSCAN clustering.
